ae605978f944999d2823ac123725bf5e2bc4412a
Andy <andrew.smith@cef.co.uk>
2022-05-09 10:30:47 +0100

    added auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
@@ -9,6 +11,15 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
+
+      <Authenticator loginMechanisms={['username']} socialProviders={['amazon', 'apple', 'facebook', 'google']} >
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+      </Authenticator>
     </div>
   );
 }
