aabadb020957d4a5075a189ed2ac882116d1924c
Andy <andrew.smith@cef.co.uk>
2022-05-09 13:56:54 +0100

    added in test paragraph

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
+        <p>test</p>
       </header>
 
       <Authenticator loginMechanisms={['username']} socialProviders={['amazon', 'apple', 'facebook', 'google']} >
